3879cc5a038e2e9535eda5e5be046b229991e7ee
zhaohongzhu <zhaohongzhu@live.com>
2016-05-25 10:37:45 +0800

    WebInit

---
+++ WebDesign/ustc-oj/ustc-oj.js
@@ -0,0 +1,38 @@
+/**
+ * Created by LY on 2016/5/11.
+ */
+
+angular.module("ustc-oj", []);
+
+angular.module("ustc-oj").controller("indexCtrl", ["$scope", function($scope){
+
+}]);
+
+angular.module("ustc-oj").controller("navbarCtrl", ["$scope", function($scope){
+
+}]);
+
+angular.module("ustc-oj").controller("probCtrl", ["$scope", function($scope){
+
+}]);
+
+angular.module("ustc-oj").controller("oneProbCtrl",  ["$scope", function($scope){
+
+}]);
+
+
+/*
+    taks1: angular作用域与数据传输问题
+
+    task2: problemList展示，problem展示
+
+    task3: submit提交设计
+        task2.1: submit提交画图
+        task2.2: submit提交展示
+
+    task4: register && login
+        task4.1: CSS实现
+        task4.2: register && login JS
+
+    task5: 背景图片选取
+ */
