c3f55deca43cb42ffb4a7a74dcd5caed38d65e52
zhaohongzhu <zhaohongzhu@live.com>
2016-12-12 20:39:04 +0800

    Login to AngularJS

--- web/js/loginCtrl.js
+++ web/js/loginCtrl.js
@@ -5,51 +5,21 @@ app.controller("loginCtrl", function ($scope, $http, $rootScope) {
             compiler: lang,
             problem_id: id
         }});
+    };
+    $scope.loginFire = function (username, pass, passagain) {
+        $http.post($rootScope.apiHost + "/api/register", {params: {
+            code: source,
+            compiler: lang,
+            problem_id: id
+        }});
+    };
+    $scope.signUpMode = false;
+
+    $scope.toLogin = function () {
+        $scope.signUpMode = false;
+    }
+
+    $scope.toSignUp = function () {
+        $scope.signUpMode = true;
     }
 });
-
-var login;
-var signup;
-
-function init()
-{
-    login = document.getElementById("login");
-    signup = document.getElementById("signup");
-
-
-    $("#signup *").prop("disabled", true);
-    $("#signup").fadeOut(0);
-    signup.style.zIndex = -1;
-}
-
-function showlogin()
-{
-    $("#login *").prop("disabled", false);
-    $("#login").fadeIn(0);
-    login.style.zIndex = 0;
-}
-
-function showsignup()
-{
-    $("#signup *").prop("disabled", false);
-    $("#signup").fadeIn(0);
-    signup.style.zIndex = 0;
-}
-
-
-function tologin()
-{
-    $("#signup *").prop("disabled", true);
-    signup.style.zIndex = -1;
-    $("#signup").fadeOut(0,showlogin());
-
-}
-
-function tosignup()
-{
-
-    $("#login *").prop("disabled", true);
-    login.style.zIndex = -1;
-    $("#login").fadeOut(0,showsignup());
-
-}
--- web/js/ustc-oj.js
+++ web/js/ustc-oj.js
@@ -6,7 +6,8 @@ var app = angular.module("ustc-oj", ['ngRoute']);
 
 app.run(function ($rootScope) {
     $rootScope.tabShow = "showProblem";
-    $rootScope.apiHost = "http://ustcoj.applinzi.com";
+    $rootScope.apiHost = "http://106.14.46.189";
+    //$rootScope.apiHost = "http://ustcoj.applinzi.com";
 })
 
 
