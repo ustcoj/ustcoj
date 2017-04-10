5d30ac5a573f7d6fb32d8006d3d530b411be4e29
volltin@live.com <volltin@live.com>
2017-04-10 21:41:31 +0800

    add contest problem

--- web1/js/admin/editContestProblemCtrl.js
+++ web1/js/admin/editContestProblemCtrl.js
@@ -3,7 +3,7 @@
  */
 angular
     .module('ustc-oj')
-    .controller("editProblemCtrl", function ($routeParams, $scope, $http, $rootScope, $window,
+    .controller("editContestProblemCtrl", function ($routeParams, $scope, $http, $rootScope, $window,
                                       networkService, problemService, adminService) {
         $scope.problem_id = null;
         $scope.problem_title = null;
@@ -16,20 +16,26 @@ angular
         $scope.description = null;
         $scope.input_description = null;
         $scope.output_description = null;
-        $scope.input_sample = null;
-        $scope.output_sample = null;
+        $scope.input_sample = [];
+        $scope.output_sample = [];
         $scope.hint = null;
+
+        $scope.data_dir = null;
+        $scope.sort_index = null;
+        $scope.contest_id = null;
+
         $scope.finishLoading = false;
 
-        if ($routeParams.problem_ID == null) {
-
+        if ($routeParams.sortIndex == null) {
+            $scope.contest_id = $routeParams.contest_ID;
         }
         else {
-            $scope.problem_id = $routeParams.problem_ID;
+            $scope.contest_id = $routeParams.contest_ID;
+            $scope.sort_index = $routeParams.sortIndex;
             adminService.getProblem(function(data){
                 var problem = data.problem;
                 adminService.Populate(problem, $scope);
-            }, $scope.problem_id, null);
+            }, $scope.sort_index, $scope.contest_id, null);
         }
 
         $scope.Fire = function () {
@@ -44,18 +50,19 @@ angular
                 description : $scope.description,
                 input_description : $scope.input_description,
                 output_description : $scope.output_description,
-                // input_sample : $scope.input_sample,
-                // output_sample : $scope.output_sample,
+                contest_id : $scope.contest_id,
+                sort_index : $scope.sort_index,
+                data_dir : $scope.data_dir,
                 hint : $scope.hint
             };
 
             adminService.arrayPopulate("input_sample", $scope.input_sample, Data);
             adminService.arrayPopulate("output_sample", $scope.output_sample, Data);
 
-            if ($scope.problem_id)
-                Data["problem_id"] = $scope.problem_id
+            // if ($scope.problem_id)
+            //     Data["problem_id"] = $scope.problem_id
 
-            adminService.addProblem(function(data){
+            adminService.addContestProblem(function(data){
                 $window.location.href = '#/';
             }, Data);
         }
